e27981bc669dffa1d138fe34d71aff593f5a7c1b
fred <abnertorres58@gmail.com>
2022-09-26 23:21:32 -0400

    Doing a refactor in order to use a service layer

--- backend/src/main/java/com/freudromero/springboot/cruddemo/rest/ContactRestController.java
+++ backend/src/main/java/com/freudromero/springboot/cruddemo/rest/ContactRestController.java
@@ -2,6 +2,7 @@ package com.freudromero.springboot.cruddemo.rest;
 
 import com.freudromero.springboot.cruddemo.dao.ContactDAO;
 import com.freudromero.springboot.cruddemo.entity.Contact;
+import com.freudromero.springboot.cruddemo.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +14,16 @@ import java.util.List;
 @RequestMapping("/api")
 public class ContactRestController {
 
-    private ContactDAO contactDAO;
-
-    // quick and dirty: inject contact dao (will refactor later)(use constructor injection)
+    private ContactService contactService;
     @Autowired
-    public ContactRestController(ContactDAO theContactDAO){
-        contactDAO = theContactDAO;
+    public ContactRestController(ContactService theContactService){
+        contactService = theContactService;
     }
 
     // expose "/contacts" and return list of contacts
     @GetMapping("/contacts")
     public List<Contact> findAll() {
-        return contactDAO.findAll();
+        return contactService.findAll();
     }
 
 }
