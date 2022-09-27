f35b300981612ec0633673f3eda026d6823ba54e
fred <abnertorres58@gmail.com>
2022-09-27 00:03:02 -0400

    REST controller methods to add and find a Contact

--- backend/src/main/java/com/freudromero/springboot/cruddemo/rest/ContactRestController.java
+++ backend/src/main/java/com/freudromero/springboot/cruddemo/rest/ContactRestController.java
@@ -4,9 +4,7 @@ import com.freudromero.springboot.cruddemo.dao.ContactDAO;
 import com.freudromero.springboot.cruddemo.entity.Contact;
 import com.freudromero.springboot.cruddemo.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,32 @@ public class ContactRestController {
     @GetMapping("/contacts")
     public List<Contact> findAll() {
         return contactService.findAll();
+    }
+
+    // add mapping for GET /contacts/{contactId}
+    @GetMapping("/contacts/{contactId}")
+    public Contact getContact(@PathVariable int contactId) {
+        Contact theContact = contactService.findById(contactId);
+
+        if(theContact == null) {
+            throw new RuntimeException("Contact id not found - " + contactId);
+        }
+
+        return theContact;
+    }
+
+    // add mapping for POST /contacts - add new contact
+    @PostMapping("/contacts")
+    public Contact addContact(@RequestBody Contact theContact) {
+
+        // also just in case they pass an id in JSON... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        theContact.setId(0);
+
+        contactService.save(theContact);
+
+        return theContact;
     }
 
 }
